cebc9567df90a5c2c79f1d35b98067f1a246a523
Dennis Macharia <denniskahiga92@gmail.com>
2026-03-14 12:14:03 +0300

    supabase intergration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,32 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes.tsx';
+import { useState, useEffect } from 'react'
+import { supabase } from '../utils/supabase'
+
+function Page() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    function getTodos() {
+      const { data: todos } = await supabase.from('todos').select()
+
+      if (todos.length > 1) {
+        setTodos(todos)
+      }
+    }
+
+    getTodos()
+  }, [])
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </div>
+  )
+}
+export default Page
 
 export default function App() {
   return <RouterProvider router={router} />;
